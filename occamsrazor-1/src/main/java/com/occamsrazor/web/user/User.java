6ada38bf6ad4154cbd8ae607c22f0f4a406020bb
pakjkwan <pakjkwan@gmail.com>
2020-04-28 18:31:31 +0900

    18:31

--- occamsrazor-1/src/main/java/com/occamsrazor/web/user/User.java
+++ occamsrazor-1/src/main/java/com/occamsrazor/web/user/User.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 
 @Getter @Setter
 public class User {
-	private String userid,passwd,name,ssn, addr;
+	private String  userid,passwd,name,ssn, addr, profile, email, phoneNumber, registerDate;
 	@Override
 	public String toString() {
 		return String.format("%s,%s,%s,%s,%s", userid,passwd,name,ssn, addr);
